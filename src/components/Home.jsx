f676ca2b0938f52b0e67e9913723babba187f5df
masnapp <masnapp2224@gmail.com>
2022-06-21 00:37:01 +0000

    spelling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,10 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Michael Snapp</h1>
             {/* <h2 className='text-gray-300'>I'm an IT Engineer</h2> */}
-            <p className='text-gray-300 py-4 max-w-[700px]'>I'm an IT professional that loves to explore technology. I'm currently 
-                learning cloud technologies and cloud engineer principles. This site was 
-                developed with React, HTML5, Tailwind CSS, Docker and Google Cloud. 
+            <p className='text-gray-300 py-4 max-w-[700px]'>I'm an IT professional that loves to learn and explore technology. I'm currently 
+                leveraging my experience and passion to learn about Cloud Engineering and DevOps. While I am not an front-end developer, I created 
+                this site with React.js to have an application to server utilizing cloud technologies. This site is hosted as a Docker container in 
+                Google Cloud Run and uses Google's Continuous Integration with Cloud Build for high availablility, elasticity, and scalability.
             </p>
             <div>
                 <button className='text-[#66FCF1] hover:text-pink-600'>View Projects <HiArrowNarrowRight /></button>
