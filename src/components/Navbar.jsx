2398d148f06505b0f80f4889152b6e75cf208c58
masnapp <masnapp2224@gmail.com>
2022-07-21 00:35:15 +0000

    add resume url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'target="_blank" rel="noopener noreferrer"
+              href='https://storage.googleapis.com/msnapp_files/public_resumes/michael_snapp_resume.pdf'target="_blank" rel="noopener noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
