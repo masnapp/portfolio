4330ab60c216e7354303ecf8587251c5296885c0
masnapp <masnapp2224@gmail.com>
2022-07-21 00:18:09 +0000

    Changes to contact and home

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/5e9c6bb3-ae2e-46ad-a16d-a21ede9f2a36" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>Submit the form below or shoow me an emaili - myemail@gmail.com</p>
+                <p className='text-gray-300 py-4'>Submit the form below or send me an emaili at myemail@gmail.com</p>
             </div>
             <input className='p-2 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='email' />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll';
+
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-[#1a1a1d]'>
@@ -9,13 +11,15 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Michael Snapp</h1>
             {/* <h2 className='text-gray-300'>I'm an IT Engineer</h2> */}
-            <p className='text-gray-300 py-4 max-w-[700px]'>I'm an IT professional that loves to learn and explore technology. I'm currently 
-                leveraging my experience and passion to learn about Cloud Engineering and DevOps. While I am not an front-end developer, I created 
-                this site with React.js to have an application to server utilizing cloud technologies. This site is hosted as a Docker container in 
-                Google Cloud Run and uses Google's Continuous Integration with Cloud Build for high availablility, elasticity, and scalability.
+            <p className='text-gray-300 py-4 max-w-[700px]'>I'm an IT professional that loves to learn about and explore new technologies. I'm currently 
+                leveraging my experience and passion to learn about Cloud Engineering and DevOps. While I am not a front-end developer, I created 
+                this site with React.js as a demonstration to utilize cloud technologies and priciples. This site is hosted as a container in 
+                Google Cloud Run and uses Google's CI/CD with Cloud Build for high availablility, elasticity, and scalability.
             </p>
             <div>
+              <Link to='projects' smooth={true} duration={500}>
                 <button className='text-[#66FCF1] hover:text-pink-600'>View Projects <HiArrowNarrowRight /></button>
+                </Link>
             </div>
         </div>
     </div>
